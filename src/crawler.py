e02330f2a606db40624f623ea91937a9344ff5c0
gorbunovakris4 <51321340+gorbunovakris4@users.noreply.github.com>
2024-05-18 12:56:20 +0300

    Update crawler.py

--- src/crawler.py
+++ src/crawler.py
@@ -1,6 +1,7 @@
 from concurrent.futures import ThreadPoolExecutor
 import logging
 import requests
+import os
 from queue import Queue, Empty
 
 from parser import HtmlDocumentParser
@@ -10,9 +11,12 @@ class Crawler:
 
     def __init__(self):
         self.parser = HtmlDocumentParser()
-        self.pool = ThreadPoolExecutor(max_workers=4)
+        self.pool = ThreadPoolExecutor(max_workers=2)
         self.visited_pages = set()
         self.pages_queue = Queue()
+        self.headers = [{"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0"},
+           {"User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"}]
+
 
     def parse_links(self, url, response):
         links = self.parser.process_page(url, response)
@@ -20,7 +24,7 @@ class Crawler:
             if link not in self.visited_pages:
                 self.pages_queue.put(link)
 
-    def post_scrape_callback(self, res):
+    def parse_results(self, res):
         result = res.result()
         if result and result[1].status_code == 200:
             self.parse_links(result[0], result[1])
@@ -29,7 +33,7 @@ class Crawler:
 
     def process_url(self, url):
         try:
-            res = requests.get(url, timeout=(3, 30))
+            res = requests.get(url, timeout=(3, 30), headers=self.headers[0])
             return url, res
         except Exception as e:
             logging.error(f"Error on getting response for {url}.\n Error message: {e}\n")
@@ -42,7 +46,7 @@ class Crawler:
                 if url not in self.visited_pages:
                     self.visited_pages.add(url)
                     job = self.pool.submit(self.process_url, url)
-                    job.add_done_callback(self.post_scrape_callback)
+                    job.add_done_callback(self.parse_results)
             except Empty:
                 return
             except Exception as e:
